a67bb8771f4c1ac7833f0ae65c4e34aba14176b6
Sabrina Fried <sabrina@photoshelter.com>
2022-03-25 09:58:43 -0400

    new file

---
+++ my_functions.js
@@ -0,0 +1,23 @@
+for (var i = 0; i < menuItems.length; i++) {
+    document.getElementsByName("quan" + i).addEventListener("change", updateCost("quan" + i));
+}
+
+function updateCost(qname) {
+    index = parseInt(String(qname.name).replace(/[^0-9]/g, ""));
+    quantity = document.getElementsByName(qname).value;
+    total = (quantity * menuItems[index].cost).toFixed(2);
+    tcost = document.getElementById(index).totalCost;
+    tcost.value = total;
+    updateTotal();
+}
+
+function updateTotal() {
+    sum = 0;
+    for (i = 0; i < menuItems.length; i++) {
+        qname = "quan" + i;
+        sum += menuItems[i].cost * document.getElementsByName(qname).value;
+    }
+    document.getElementById("subtotal").value = sum;
+    document.getElementById("tax").value = (0.0625 * document.getElementById("subtotal").value).toFixed(2);
+    document.getElementById("total").value = document.getElementById("subtotal").value + document.getElementById("tax").value;
+}
