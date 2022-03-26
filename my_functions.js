871a2f027f72c97a6b7281d59706bac2a14514cc
Sabrina Fried <sabrina@photoshelter.com>
2022-03-25 21:46:08 -0400

    lets try this

--- my_functions.js
+++ my_functions.js
@@ -1,9 +1,8 @@
 /*my functions to implement Jade Delight assignment specifications*/
 
-
-
+var quans = document.getElementsByClassName(selectQuantity);
 for (var i = 0; i < menuItems.length; i++) {
-    document.getElementsByName("quan" + i).addEventListener("change", updateCost("quan" + i));
+    quans[i].addEventListener("change", updateCost("quan" + i));
 }
 
 document.getElementsByValue("delivery").addEventListener("load", reveal(document.getElementsByClassName("userInfo address")));
@@ -22,7 +21,7 @@ function updateCost(qname = "")
 {
     index = parseInt(String((qname).replace(/[^0-9]/g, "")));
     console.log(qname + "; " + document.getElementsByName(qname));
-    quantity = parseInt(document.getElementsByName(qname).value);
+    quantity = parseInt(document.getElementsByName(qname));
     console.log(quantity);
     total = (quantity * menuItems[index].cost).toFixed(2);
     tcost = document.getElementById(index);
