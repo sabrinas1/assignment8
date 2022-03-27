e94bb1507815acf518e7810f0a65a75bf7388230
Sabrina Fried <sabrina@photoshelter.com>
2022-03-27 11:51:08 -0400

    inner

--- my_functions.js
+++ my_functions.js
@@ -23,7 +23,7 @@ function updateCost(qname = "")
     quantity = document.getElementById(qname).value;
     total = (quantity * menuItems[index].cost).toFixed(2);
     tcost = document.getElementById(index);
-    console.log(tcost + "; " + tcost.totalCost);
+    console.log(tcost.innerHTML);
     tcost.totalCost.setAttribute("value", total);
     updateTotal();
 }
