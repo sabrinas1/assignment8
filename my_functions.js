9ea462651aaf696f778a5de459bb4ad8a13c6e8f
Sabrina Fried <sabrina@photoshelter.com>
2022-04-03 13:47:24 -0400

    valid

--- my_functions.js
+++ my_functions.js
@@ -1,7 +1,7 @@
 /*my functions to implement Jade Delight assignment specifications*/
 
 document.querySelector("input[value='delivery']").addEventListener("click", reveal(document.getElementsByClassName("address")));
-//document.addEventListener("submit", verify(document.getElementsByTagName("form")))
+document.addEventListener("submit", verify(document.getElementsByTagName("form")))
 
 function reveal(obj)
 {
@@ -42,29 +42,52 @@ function updateTotal()
     document.getElementById("total").setAttribute("value", sum + ttax);
 }
 
-// function verify(form = {})
-// {
-//     last = form.getElementsByClassName("userInfo").lname.getAttribute("value");
-//     if (last == null) {
-//         alert("Please enter your last name");
-//         return;
-//     }
-//     phone = form.getElementsByClassName("userInfo").phone.getAttribute("value");
-//     phone = String(phone.replace(/[^0-9]/g, ""))
-//     if (phone.length() != 7 || phone.length() != 10) {
-//         alert("Please enter a valid phone number");
-//         return;
-//     }
+function verify(form = {})
+{
+    last = form.getElementsByClassName("userInfo").lname.getAttribute("value");
+    if (last == null) {
+        alert("Please enter your last name");
+        return;
+    }
+    phone = form.getElementsByClassName("userInfo").phone.getAttribute("value");
+    phone = String(phone.replace(/[^0-9]/g, ""))
+    if (phone.length() != 7 || phone.length() != 10) {
+        alert("Please enter a valid phone number");
+        return;
+    }
 
-//     currTime = new Date;
+    currTime = new Date.now();
+    currHour = currTime.getHours();
+    currMinute = currTime.getMinutes();
+
+    method = form.p_or_d[0];
+    if (method.checked === "checked") {
+        newMin = currMinute + 15;
+    } else {
+        newMin = currMinute + 30;
+        street = form.getElementsByClassName("userInfo").street.getAttribute("value");
+        city = form.getElementsByClassName("userInfo").city.getAttribute("value");
+        if (street == null || city == null) {
+            alert("Please enter your address");
+            return;
+        }
+    }
+
+    if (newMin > 60) {
+        newMin -= 60;
+        newHour = currHour + 1;
+    }
+
+    total = form.getElementById(total).value;
+    if (total == 0) {
+        alert("Invalid Order: No items selected");
+        return;
+    }
+
+    alert("Thank you for ordering from Jade Delight!");
+
+    //window.open("summary.html");
 
 
-//     //get time (date)
-//     //if pickup -> +15
-//     //if delivery -> +30
-//         //street + city
-//     //at least 1 item (total not 0)
-//     //if issues, alert
-//     //no issues -> popup thanks
-//         //new window with item, quantity, total (for item), subtotal, tax, total, time
-// }
+    
+}
