a560e0952f5bf06ba4af726b6ee2be76bbfb0815
Sabrina Fried <sabrina@photoshelter.com>
2022-04-03 12:22:11 -0400

    hidden

--- my_functions.js
+++ my_functions.js
@@ -1,17 +1,18 @@
 /*my functions to implement Jade Delight assignment specifications*/
 
-document.addEventListener("load", reveal(document.getElementsByClassName("address").street));
-document.addEventListener("load", reveal(document.getElementsByClassName("address").city));
-document.querySelector("input[value='delivery']").addEventListener("click", reveal(document.getElementsByClassName("address").street));
-document.querySelector("input[value='delivery']").addEventListener("click", reveal(document.getElementsByClassName("address").city));
+document.addEventListener("load", reveal(document.getElementsByClassName("address")));
+document.addEventListener("load", reveal(document.getElementsByClassName("address"));
+document.querySelector("input[value='delivery']").addEventListener("click", reveal(document.getElementsByClassName("address")));
+document.querySelector("input[value='delivery']").addEventListener("click", reveal(document.getElementsByClassName("address")));
+//document.addEventListener("submit", verify(document.getElementsByTagName("form")))
 
 function reveal(obj = {})
 {
     console.log("Revealing/ hiding!");
-    if (obj.type != "hidden") {
-        obj.type = "hidden";
-    } else if (obj.type === "hidden") {
-        obj.type = "text";
+    if (obj.style != "visibility: hidden") {
+        obj.style = "visibility: hidden";
+    } else if (obj.style === "visibility: hidden") {
+        obj.style = "visibility: visible";
     }
 }
 
@@ -43,3 +44,30 @@ function updateTotal()
     document.getElementById("tax").setAttribute("value", ttax);
     document.getElementById("total").setAttribute("value", sum + ttax);
 }
+
+// function verify(form = {})
+// {
+//     last = form.getElementsByClassName("userInfo").lname.getAttribute("value");
+//     if (last == null) {
+//         alert("Please enter your last name");
+//         return;
+//     }
+//     phone = form.getElementsByClassName("userInfo").phone.getAttribute("value");
+//     phone = String(phone.replace(/[^0-9]/g, ""))
+//     if (phone.length() != 7 || phone.length() != 10) {
+//         alert("Please enter a valid phone number");
+//         return;
+//     }
+
+//     currTime = new Date;
+
+
+//     //get time (date)
+//     //if pickup -> +15
+//     //if delivery -> +30
+//         //street + city
+//     //at least 1 item (total not 0)
+//     //if issues, alert
+//     //no issues -> popup thanks
+//         //new window with item, quantity, total (for item), subtotal, tax, total, time
+// }
